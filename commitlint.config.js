ea1f4ee4ae3cd6cca8ac7976d3f999ccd9a0211a
amin75t <167482540+amin75t-work@users.noreply.github.com>
2025-07-14 03:42:37 +0330

    fixed: fix huskey

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,15 +4,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      [
-        "docs",
-        "marge",
-        "update",
-        "ci",
-        "fixed",
-        "refactor",
-        "test",
-      ],
+      ["docs", "marge", "update", "ci", "fixed", "refactor", "test", "init"],
     ],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
